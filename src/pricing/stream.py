4cd694e881676475ed60964c5d92c22c94241c91
canl <can_lu@hotmail.com>
2020-05-23 07:02:47 +0800

    streaming real time price

---
+++ src/pricing/stream.py
@@ -0,0 +1,28 @@
+import json
+import logging
+
+import oandapyV20.endpoints.pricing as pricing
+
+from src.env import PRACTICE_ENV
+
+logger = logging.getLogger(__name__)
+account = PRACTICE_ENV.account()
+env = PRACTICE_ENV
+api = PRACTICE_ENV.api()
+
+logger.info('hello')
+
+params = {
+    "instruments": "EUR_USD,EUR_JPY"
+}
+
+max_recs = 100
+r = pricing.PricingStream(accountID=account.mt4, params=params)
+rv = api.request(r)
+
+# print(r.response)
+maxrecs = 100
+for tick in rv:
+    print(json.dumps(r, indent=4), ",")
+    if maxrecs == 0:
+        r.terminate("maxrecs records received")
