65a5768dcc68f52d2fd9a33edeb72e6c8b345b05
canl <can_lu@hotmail.com>
2020-05-23 08:10:06 +0800

    streaming real-time prices

--- src/pricing/stream.py
+++ src/pricing/stream.py
@@ -1,7 +1,10 @@
 import json
 import logging
+import time
 
-import oandapyV20.endpoints.pricing as pricing
+from oandapyV20 import V20Error
+from oandapyV20.endpoints.pricing import PricingStream
+from oandapyV20.exceptions import StreamTerminated
 
 from src.env import PRACTICE_ENV
 
@@ -10,19 +13,41 @@ account = PRACTICE_ENV.account()
 env = PRACTICE_ENV
 api = PRACTICE_ENV.api()
 
-logger.info('hello')
 
-params = {
-    "instruments": "EUR_USD,EUR_JPY,GBP_USD"
-}
+class StreamingPrices:
+    def __init__(self, instruments, callback, max_rec=None):
+        self.instruments = instruments
+        self.max_rec = max_rec
+        self.callback = callback
 
-max_recs = 100
-r = pricing.PricingStream(accountID=account.mt4, params=params)
+    def run(self):
+        c = 0
+        r = PricingStream(accountID=account.mt4, params={"instruments": ",".join(self.instruments)})
+        try:
+            for ticks in api.request(r):
+                self.callback(ticks)
+                c += 1
+                if self.max_rec and c >= self.max_rec:
+                    r.terminate(f"Maximum heartbeats received: {c}")
+        except V20Error as e:
+            # catch API related errors that may occur
+            logger.error(f"V20Error: {e}")
 
-maxrecs = 10
-n = 0
-for ticks in api.request(r):
-    print(json.dumps(ticks, indent=4), ",")
-    n += 1
-    if n >= maxrecs:
-        r.terminate("maxrecs records received")
+        except ConnectionError as e:
+            logger.error(f"ConnectionError: {e}")
+            time.sleep(2)
+
+        except StreamTerminated as e:
+            logger.error(f"StreamTerminated: {e}")
+
+        except Exception as e:
+            logger.error(f"Some exception: {e}")
+
+
+if __name__ == '__main__':
+    def print_data(message):
+        print(json.dumps(message, indent=4), ",")
+
+
+    sp = StreamingPrices(['GBP_USD', 'EUR_USD'], print_data, max_rec=10)
+    sp.run()
