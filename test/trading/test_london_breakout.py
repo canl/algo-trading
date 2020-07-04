c17a574f7451f6646a726824bb8e16fc43e991ba
canl <can_lu@hotmail.com>
2020-07-04 09:51:55 +0800

    fixed the unittests

--- test/trading/test_london_breakout.py
+++ test/trading/test_london_breakout.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 
-from src.trading.london_breakout import get_risk_pct
+from src.order_utils.order_manager import OrderManager
+from src.trading.london_breakout import LondonBreakout
 
 
 class TestLondonBreakout(TestCase):
+    def setUp(self) -> None:
+        om = OrderManager(account='mt4')
+        self.trader = LondonBreakout(om)
+
     def test_get_risk_pct(self):
         test_cases = [
             (0.03, [{'id': 1, 'realized_pl': 100}]),
@@ -18,4 +23,4 @@ class TestLondonBreakout(TestCase):
             (0.01, [{'id': 1, 'realized_pl': 100}, {'id': 2, 'realized_pl': 100}, {'id': 3, 'realized_pl': 100}, {'id': 4, 'state': 'OPEN', 'realized_pl': -100}, {'id': 4, 'realized_pl': 100}]),
         ]
         for expect, trans in test_cases:
-            self.assertEqual(expect, get_risk_pct(trans))
+            self.assertEqual(expect, self.trader.get_risk_pct(trans))
