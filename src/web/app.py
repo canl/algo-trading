4a57cdce1af03909205a17b8099eceee7eb8c6e1
canl <can_lu@hotmail.com>
2020-07-10 22:28:56 +0800

    Initial sign-in screen

--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,12 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
 
+@app.route('/')
+def index():
+    return render_template('index.html', title='Signin')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
