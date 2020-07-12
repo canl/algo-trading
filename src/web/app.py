158a6eda48651f18a88c55810fb64ed6016b9f6a
canl <can_lu@hotmail.com>
2020-07-12 23:05:21 +0800

    move the app out to make wsgi to work

--- src/web/app.py
+++ src/web/app.py
@@ -5,10 +5,10 @@ from src.web.db import db
 from src.web.auth import auth as auth_blueprint
 from src.web.main import main as main_blueprint
 
+app = Flask(__name__)
+
 
 def create_app():
-    app = Flask(__name__)
-
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
