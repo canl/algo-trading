740f97842d014581d3289b307d48c1bf10fb2407
canl <can_lu@hotmail.com>
2020-08-09 17:54:47 +0800

    Changed the card background color

--- src/web/static/js/charts/chart-pie-demo.js
+++ src/web/static/js/charts/chart-pie-demo.js
@@ -1,16 +1,66 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
+var data = {
+  datasets: [{
+    data: [
+      42, 58
+    ],
+    backgroundColor: [
+      "#FF6384",
+      "#4BC0C0"
+    ],
+    label: 'My dataset' // for legend
+  }],
+  labels: [
+    "Win",
+    "Loss",
+  ]
+};
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
-    datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
+var pieOptions = {
+  events: false,
+  legend: {
+    "display": false
   },
+  animation: {
+    duration: 500,
+    easing: "easeOutQuart",
+    onComplete: function () {
+      var ctx = this.chart.ctx;
+      ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'bottom';
+
+      this.data.datasets.forEach(function (dataset) {
+
+        for (var i = 0; i < dataset.data.length; i++) {
+          var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
+            total = dataset._meta[Object.keys(dataset._meta)[0]].total,
+            mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius) / 2,
+            start_angle = model.startAngle,
+            end_angle = model.endAngle,
+            mid_angle = start_angle + (end_angle - start_angle) / 2;
+
+          var x = mid_radius * Math.cos(mid_angle);
+          var y = mid_radius * Math.sin(mid_angle);
+
+          ctx.fillStyle = '#fff';
+          if (i === 3) { // Darker text color for lighter background
+            ctx.fillStyle = '#444';
+          }
+          var percent = String(Math.round(dataset.data[i] / total * 100)) + "%";
+          //Don't Display If Legend is hide or value is 0
+          if (dataset.data[i] != 0 && dataset._meta[0].data[i].hidden != true) {
+            ctx.fillText(dataset.data[i], model.x + x, model.y + y);
+            // Display percent in another line, line break doesn't work for fillText
+            ctx.fillText(percent, model.x + x, model.y + y + 15);
+          }
+        }
+      });
+    }
+  }
+};
+var pieChartCanvas = document.getElementById("pieChart");
+var pieChart = new Chart(pieChartCanvas, {
+  type: 'pie', // or doughnut
+  data: data,
+  options: pieOptions
 });
--- src/web/static/js/scripts.js
+++ src/web/static/js/scripts.js
@@ -4,58 +4,58 @@
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
  */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Add active state to sidebar nav links
-    const path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-    $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
-        if (this.href === path) {
-            $(this).addClass("active");
-            $(this).parents().removeClass('collapse');
-        }
-    });
+  // Add active state to sidebar nav links
+  const path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+  $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+    if (this.href === path) {
+      $(this).addClass("active");
+      $(this).parents().removeClass('collapse');
+    }
+  });
 
-    // Toggle the side navigation
-    $("#sidebarToggle").on("click", function (e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-        $(".sidebar").toggleClass("toggled");
-    });
+  // Toggle the side navigation
+  $("#sidebarToggle").on("click", function (e) {
+    e.preventDefault();
+    $("body").toggleClass("sb-sidenav-toggled");
+    $(".sidebar").toggleClass("toggled");
+  });
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ===
-            this.pathname.replace(/^\//, "") &&
-            location.hostname === this.hostname
-        ) {
-            let target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ===
+      this.pathname.replace(/^\//, "") &&
+      location.hostname === this.hostname
+    ) {
+      let target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-    $(document).scroll(function () {
-        const scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $(".scroll-to-top").fadeIn();
-        } else {
-            $(".scroll-to-top").fadeOut();
-        }
-    });
+  $(document).scroll(function () {
+    const scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $(".scroll-to-top").fadeIn();
+    } else {
+      $(".scroll-to-top").fadeOut();
+    }
+  });
 })(jQuery);
 
 function numberWithCommas(x) {
-    let parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
+  let parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
 }
