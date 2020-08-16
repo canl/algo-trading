e11a3a51952ee9a254e45dea8367cee5d082c571
canl <can_lu@hotmail.com>
2020-08-16 15:15:13 +0800

    Moved sqlite db files to project level

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,18 +1,19 @@
 # init.py
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir + "/../db", 'db.sqlite')
 
     db.init_app(app)
 
