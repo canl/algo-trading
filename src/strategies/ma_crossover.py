888ebcdafc0340e373eb8c594ec2e241fecd1506
canl <can_lu@hotmail.com>
2020-08-22 19:48:32 +0800

    minor change to macd_crossover.py

--- src/strategies/ma_crossover.py
+++ src/strategies/ma_crossover.py
@@ -1,3 +1,4 @@
+import queue
 from datetime import datetime, timedelta
 
 import matplotlib.pyplot as plt
@@ -5,11 +6,15 @@ import numpy as np
 import pandas as pd
 
 from src.db.ohlc_to_db import connect_to_db
+from src.finta.ta import TA
+from src.indicators import average_true_range, exponential_moving_average
 from src.pricer import read_price_df
 from src.orders.order import Order, OrderStatus
 
+
 # Tested with real tick data
 # Rules: Simple MA cross over strategy, can be used in either 1 hour or 1 day timeframe
+# Maximum one oder allowed
 #   Define short and long window
 #   Long condition:
 #       when short window cross over long window from bottom
@@ -26,61 +31,30 @@ from src.orders.order import Order, OrderStatus
 #       when short window cross over long window from bottom
 
 
-short_window = 100
-long_window = 350
+# short_window = 100
+# long_window = 350
+#
+# last_date = datetime.today() - timedelta(days=1)
+# start_date = last_date - timedelta(days=3000)
 
-last_date = datetime.today() - timedelta(days=1)
-start_date = last_date - timedelta(days=3000)
 
+# 1. Read 1 hour price with standard atr
+# 2. Generate buy/sell signal when crossover
+# 3. Simulate ticking price and placing orders
 
-def generate_signals():
-    df = read_price_df(instrument='GBP_USD', granularity='D', start=start_date, end=last_date)
-
-    df['long_short'] = 0
-    df['short_mavg'] = df['close'].rolling(window=short_window, min_periods=1, center=False).mean()
-    df['long_mavg'] = df['close'].rolling(window=long_window, min_periods=1, center=False).mean()
-    df['long_short'][short_window:] = np.where(df['short_mavg'][short_window:] >= df['long_mavg'][short_window:], 1, 0)
-
-    df['signal'] = df['long_short'].diff()
-    print(df[['open', 'close', 'short_mavg', 'long_mavg', 'signal', 'long_short']])
-    df.to_csv(r'C:\temp\prices.csv')
+def generate_signals(instrument: str = "GBP_USD", short_win: int = 16, long_win: int = 64,
+                     start_date: datetime = datetime(2019, 1, 1, 0, 0, 0), end_date: datetime = datetime(2020, 1, 1, 0, 0, 0)) -> pd.DataFrame:
+    df = read_price_df(instrument=instrument, granularity='H1', start=start_date, end=end_date)
+    df['ema_short'] = exponential_moving_average(df, short_win)
+    df['ema_long'] = exponential_moving_average(df, long_win)
+    df['atr'] = average_true_range(df, 14)
+    df['signal'] = 0.0
+    # Create a 'signal' (invested or not invested) when the short moving average crosses the long
+    # moving average, but only for the period greater than the shortest moving average window
+    df['signal'][short_win:] = np.where(df['ema_short'][short_win:] > df['ema_long'][short_win:], 1.0, 0.0)
+    # Take the difference of the signals in order to generate actual trading orders
+    df['positions'] = df['signal'].diff()
     return df
-
-
-def plot(df):
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, ylabel='Price in $')
-
-    df['close'].plot(ax=ax1, color='r', lw=2.)
-
-    df[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
-
-    ax1.plot(df.loc[df.signal == 1.0].index, df.short_mavg[df.signal == 1.0], '^', markersize=10, color='m')
-    ax1.plot(df.loc[df.signal == -1.0].index, df.short_mavg[df.signal == -1.0], 'v', markersize=10, color='k')
-
-    plt.show()
-
-
-def back_test():
-    df = generate_signals()
-    price_data = df.reset_index().to_dict('records')
-
-    next_move = None
-    orders = []
-    for p in price_data:
-        if not np.isnan(p.get('signal')) and p.get('signal') != 0:
-            next_move = 'buy' if p.get('signal') == 1 else 'sell'
-            continue
-
-        if next_move:
-            if orders:
-                orders[-1] = Order(orders[-1].order_date, orders[-1].side, orders[-1].entry, status=OrderStatus.CLOSED,
-                                   pnl=(p.get('close') - orders[-1].entry) * (1 if orders[-1].side == 'buy' else -1) * 10000)
-            orders.append(Order(pd.to_datetime(p.get('time')), next_move, p.get('open'), status=OrderStatus.FILLED))
-            next_move = None
-
-    # plot(df)
-    return orders
 
 
 def retrieve_price():
@@ -89,22 +63,18 @@ def retrieve_price():
     db_path = os.path.join(basedir + "/../db", 'db.sqlite')
     conn = connect_to_db(db_path)
     cur = conn.cursor()
-    cur.execute("select * from gbp_ohlc where date(time) >= '2010-01-04'")
+    cur.execute("select * from gbp_ohlc where date(time) between '2020-01-01' and '2020-07-31'")
 
     for row in cur.fetchall():
-        print(row)
+        yield row
 
+
+price_events = queue.Queue()
 
 if __name__ == '__main__':
-    # orders = back_test()
-    # test_result = pd.DataFrame(
-    #     {
-    #         'date': [o.order_date for o in orders],
-    #         'pnl': [o.pnl for o in orders],
-    #     }
-    # ).set_index('date')
-    # test_result['cumsum'] = test_result['pnl'].cumsum()
-    # print(test_result)
-    # test_result['cumsum'].plot()
-    # plt.show()
-    retrieve_price()
+    signals = generate_signals(start_date=datetime(2020, 1, 1, 0, 0, 0), end_date=datetime(2020, 7, 31, 0, 0, 0))
+    # signals.to_csv(r"C:\temp\signals-2.csv")
+    print(signals[signals.positions != 0])
+
+    for ohlc in retrieve_price():
+        price_events.put(ohlc)
