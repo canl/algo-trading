bd3d6c9ba3b1b5c113fc59ee2b6f83f54eaad6dc
canl <can_lu@hotmail.com>
2020-09-08 00:05:27 +0800

    added bb and rsi strategy

---
+++ src/strategies/bb_rsi.py
@@ -0,0 +1,27 @@
+"""
+Time Frame:15 or higher.
+
+Pairs:all
+
+Indicators:
+
+Bollinger Bands (2, 20);
+
+RSI (11)
+
+Long Entry:
+When the RSI drops above 70, the RSI drops above the 'higher Bollinger Band' and when the Price drops above the 'HigherBollinger Band'.
+
+Short Entry;
+When the RSI drops below 30, the RSI drops below the 'Lower Bollinger Band' and when the Price drops below the 'Lower Bollinger Band'.
+
+Exit Order
+
+Profit Target:
+    15min: EUR/USD, 9 pips GBP/USD 12 pips
+    30min: EUR/USD, 14 pips GBP/USD 18 pips;
+    H4:60 pips EUR/USD, 70 pips GBP/USD ;
+    Daily: 200pips EUR/USD , 250 pips GBP/USD ;
+
+Stop loss; 5 pips below or above of the entry bar.
+"""
